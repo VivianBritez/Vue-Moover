974d3a1a1c0474597e84bda855777dcaf6d55554
Vivian Britez <vivianbritez91@gmail.com>
2021-03-06 12:07:12 -0500

    Inicialización del proyecto

---
+++ moover/moover-web/src/App.vue
@@ -0,0 +1,21 @@
+<template lang= 'pug'>
+  #app
+    img(src='./assets/logo.png')
+    h1 {{ msg }}
+    
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome Moover app'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import './scss/main.scss'
+</style>
